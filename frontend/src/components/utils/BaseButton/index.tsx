8add75136802da8945230833611cabfa7632ec09
SuuCH <ntrikrik1027@gmail.com>
2021-11-04 03:27:55 +0900

    [feat] ボタンコンポーネントの追加

---
+++ frontend/src/components/utils/BaseButton/index.tsx
@@ -0,0 +1,25 @@
+import type { MouseEventHandler, VFC } from "react";
+import { Button } from "@material-ui/core";
+
+interface Props {
+  text: string;
+  color?: string;
+  onClick: MouseEventHandler<HTMLButtonElement> | undefined;
+}
+
+const BaseButton: VFC<Props> = ({ text, color, onClick}) => {
+  return (
+    <Button
+      style={{
+        backgroundColor: color,
+        borderRadius: "20px 20px 20px 20px",
+        color: "white",
+      }}
+      onClick={onClick}
+    >
+      {text}
+    </Button>
+  );
+};
+
+export { BaseButton };
--- frontend/src/pages/login/index.tsx
+++ frontend/src/pages/login/index.tsx
@@ -1,10 +1,17 @@
-import type { VFC } from "react";
+import { BaseButton } from "../../components/utils/BaseButton";
 import { Form } from "../../components/login/Form";
+import type { VFC } from "react";
 
-const Login: VFC = () => (
-  <>
+const Login: VFC = () => {
+  const handleOnClickLoginButton = () : void  => {
+    console.log("push");
+  }
+  return(
+    <>
     <Form />
-  </>
-);
+      <BaseButton text="ログイン" color="#06681B" onClick={handleOnClickLoginButton}/>
+    </>
+  );
+}
 
-export {Login};
+export { Login };
