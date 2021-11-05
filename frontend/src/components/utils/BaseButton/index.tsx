7af98af1ed4bf36e6222d8d2a75ccc9f892c1018
SuuCH <ntrikrik1027@gmail.com>
2021-11-06 00:57:34 +0900

    [feat] ログイン画面でのボタンのスタイルの調整

--- frontend/src/components/utils/BaseButton/index.tsx
+++ frontend/src/components/utils/BaseButton/index.tsx
@@ -1,22 +1,25 @@
 import type { MouseEventHandler, VFC } from "react";
+import clsx from "clsx";
 import { Button } from "@material-ui/core";
 
 interface Props {
+  className?: string;
   text: string;
   color?: string;
   onClick: MouseEventHandler<HTMLButtonElement> | undefined;
 }
 
-const BaseButton: VFC<Props> = ({ text, color, onClick }) => {
+const BaseButton: VFC<Props> = ({ text, className , color, onClick ,}) => {
   return (
     <>
       <Button
+        className={clsx(className)}
+        onClick={onClick}
         style={{
           backgroundColor: color,
           borderRadius: "20px 20px 20px 20px",
           color: "white",
         }}
-        onClick={onClick}
       >
         {text}
       </Button>
