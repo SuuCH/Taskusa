f6c4e94a0a8f6a4124496dee108dc8ca4f1f6e2d
SuuCH <ntrikrik1027@gmail.com>
2021-11-06 00:55:50 +0900

    [feat] フォームのスタイルの調整

--- frontend/src/components/login/Form/index.tsx
+++ frontend/src/components/login/Form/index.tsx
@@ -1,17 +1,24 @@
 import type { VFC } from "react";
 import { TextField } from "@material-ui/core";
+import styles from "./index.module.css";
 
 const Form: VFC = () => {
   return (
     <>
-      <form noValidate autoComplete="off">
-        <TextField id="standard-basic" label="E-mail" style={{ backgroundColor: "white" }}/>
+      <form autoComplete="off" className={styles.form} noValidate>
         <TextField
+          className={styles.textForm}
+          id="standard-basic"
+          label="E-mail"
+          style={{ backgroundColor: "white", margin: "25px 25px 0 25px" }}
+        />
+        <TextField
+          autoComplete="current-password"
+          className={styles.textForm}
           id="standard-password-input"
           label="Password"
+          style={{ backgroundColor: "white", margin: "25px 25px 0 25px" }}
           type="password"
-          autoComplete="current-password"
-          style={{ backgroundColor: "white" }}
         />
       </form>
     </>
