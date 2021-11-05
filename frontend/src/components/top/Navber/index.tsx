5932310060a4a1d6bbcac8627c6f30fd48985894
RikuFujii <60252699+SuuCH@users.noreply.github.com>
2021-11-06 01:03:31 +0900

    [feat] ナビバーを配置してstyleの調整 (#11)

--- frontend/src/components/top/Navber/index.tsx
+++ frontend/src/components/top/Navber/index.tsx
@@ -1,19 +1,37 @@
 import type { VFC } from "react";
-import {AppBar, Box, Toolbar} from "@material-ui/core";
+import { AppBar, Box, Toolbar } from "@material-ui/core";
+import { BaseButton } from "../../utils/BaseButton";
+import { useNavigate } from "react-router-dom";
+import LogoTaskusa from "../../../images/LogoTaskusa.png";
+import styles from "./index.module.css";
 
 const Navber: VFC = () => {
+  const navigate = useNavigate();
+  const handleOnClickLogoutButton = (): void => {
+    console.log("push");
+    navigate("/");
+  };
   return (
     <>
-      <Box style={{ flexGrow: 1}}>
-        <AppBar style={{ position: "static"}}>
-          <Toolbar style={{ backgroundColor: "#DAF8E7", minHeight:"55px"}}>
-            {/*ダミーボタン*/}
-            <button>logout</button>
+      <Box style={{ flexGrow: 1 }}>
+        <AppBar style={{ position: "static" }}>
+          <Toolbar
+            style={{ backgroundColor: "#DAF8E7", minHeight: "55px" }}
+            className={styles.navber}
+          >
+            <img src={LogoTaskusa} alt="TaskusaLogo" className={styles.image} />
+            <div className= {styles.button}>
+              <BaseButton
+                text="ログアウト"
+                color="#06681B"
+                onClick={handleOnClickLogoutButton}
+              />
+            </div>
           </Toolbar>
         </AppBar>
       </Box>
     </>
   );
-}
+};
 
-export {Navber};
+export { Navber };
