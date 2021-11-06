6a281269ce58ee0ca8c073b160e765ea450b3434
halpi17 <nishiyamaymyt@gmail.com>
2021-11-06 22:50:29 +0900

    [feat] 草カレンダーの仮実装・配置

---
+++ frontend/src/components/top/Calendar/index.tsx
@@ -0,0 +1,85 @@
+import type { VFC } from "react";
+import CalendarHeatmap from "react-calendar-heatmap";
+import ReactTooltip from "react-tooltip";
+
+import "react-calendar-heatmap/dist/styles.css";
+import styles from "./index.module.css";
+
+const today = new Date();
+
+interface DateAndCount {
+  date: Date;
+  count: number;
+}
+
+const Calendar: VFC = () => {
+  const randomValues = getRange(365).map((index) => {
+    return {
+      date: shiftDate(today, -index),
+      count: getRandomInt(0, 5),
+    };
+  });
+
+  return (
+    <>
+      <div className={styles.calendar}>
+        <CalendarHeatmap
+          startDate={shiftDate(today, -364)}
+          endDate={today}
+          values={
+            randomValues
+            //   [
+            //   { date: "2021-07-03", count: 1 },
+            //   { date: "2021-08-22", count: 2 },
+            //   { date: "2021-07-29", count: 4 },
+            //   { date: "2021-10-01", count: 1 },
+            //   { date: "2021-10-03", count: 2 },
+            //   { date: "2021-10-06", count: 3 },
+            //   { date: "2021-10-10", count: 4 },
+            //   { date: "2021-10-07", count: 5 },
+            //   { date: "2021-09-15", count: 4 },
+            //   { date: "2021-11-06", count: 0 },
+            //   // ...and so on
+            // ]
+          }
+          classForValue={
+            (value: DateAndCount) =>
+              `color-grass-${value.count < 5 ? value.count : "5"}`
+            // if (!value) {
+            //   return "color-empty";
+            // }
+            // return `color-grass-${value.count}`;
+          }
+          tooltipDataAttrs={(value: DateAndCount) => {
+            return {
+              "data-tip": `${value.date
+                .toISOString()
+                .slice(0, 10)} has count: ${value.count}`,
+            };
+          }}
+          showWeekdayLabels={true}
+          onClick={(value) =>
+            alert(`Clicked on value with count: ${value.count}`)
+          }
+        />
+        <ReactTooltip />
+      </div>
+    </>
+  );
+};
+
+export { Calendar };
+
+const shiftDate = (date: Date, numDays: number): Date => {
+  const newDate = new Date(date);
+  newDate.setDate(newDate.getDate() + numDays);
+  return newDate;
+};
+
+const getRange = (count: number): number[] => {
+  return Array.from({ length: count }, (_, i) => i);
+};
+
+const getRandomInt = (min: number, max: number): number => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
--- frontend/src/pages/top/index.tsx
+++ frontend/src/pages/top/index.tsx
@@ -1,10 +1,12 @@
 import type { VFC } from "react";
+import { Calendar } from "../../components/top/Calendar";
 import { Navber } from "../../components/top/Navber";
 
 const Top: VFC = () => {
   return (
     <>
       <Navber />
+      <Calendar />
     </>
   );
 };
