eb37f04ca0cc97fb2958d81e1c3adde28d73f6b8
halpi <81910588+halpi17@users.noreply.github.com>
2021-11-11 03:19:50 +0900

    [feat] 未完了・予定・完了済タブの実装 (#15)
    * [chore] @material-ui/icons の追加
    
    * [feat] 完了・予定・未完了タブの仮実装
    
    * [refactor] 別のコンポーネントとしてファイルを分割した
    
    * [feat] タブの色分け実装
    
    * [refactor] 不要コメントの削除

--- frontend/src/pages/top/index.tsx
+++ frontend/src/pages/top/index.tsx
@@ -1,12 +1,14 @@
 import type { VFC } from "react";
 import { Calendar } from "../../components/top/Calendar";
 import { Navber } from "../../components/top/Navber";
+import { TaskTabs } from "../../components/top/TaskTabs";
 
 const Top: VFC = () => {
   return (
     <>
       <Navber />
       <Calendar />
+      <TaskTabs />
     </>
   );
 };
