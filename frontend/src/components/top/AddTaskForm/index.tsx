74d7c5565bb08afdccc6a0c31db560fd780fac96
SuuCH <ntrikrik1027@gmail.com>
2021-11-14 02:38:20 +0900

    [chore] スタイルの調整

--- frontend/src/components/top/AddTaskForm/index.tsx
+++ frontend/src/components/top/AddTaskForm/index.tsx
@@ -18,31 +18,34 @@ const AddTaskForm: VFC = () => {
   };
   return (
     <>
-      <div className={styles.formWrapper}>
-        <form>
-          <TextField
-            required
-            hiddenLabel
-            placeholder="タスクを入力してください"
-            variant="outlined"
-            style={{ width: "50%" }}
+      <div className={styles.wrapper}>
+        <div className={styles.formWrapper}>
+          <form className={styles.form}>
+            <TextField
+              className={styles.textField}
+              required
+              hiddenLabel
+              placeholder="タスクを入力してください"
+              variant="outlined"
+            />
+          </form>
+          <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <KeyboardDatePicker
+              disableToolbar
+              label="日付入力"
+              variant="inline"
+              format="yyyy/MM/dd"
+              value={date}
+              onChange={handleChangeDate}
+            />
+          </MuiPickersUtilsProvider>
+          <BaseButton
+            className={styles.button}
+            color="#24C075"
+            text="追加"
+            onClick={handleClickTaskAddButton}
           />
-        </form>
-        <MuiPickersUtilsProvider utils={DateFnsUtils}>
-          <KeyboardDatePicker
-            disableToolbar
-            label="日付入力"
-            variant="inline"
-            format="yyyy/MM/dd"
-            value={date}
-            onChange={handleChangeDate}
-          />
-        </MuiPickersUtilsProvider>
-        <BaseButton
-          color="#24C075"
-          text="追加"
-          onClick={handleClickTaskAddButton}
-        />
+        </div>
       </div>
     </>
   );
