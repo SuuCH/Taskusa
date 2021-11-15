b9fa8eebb99b45441e09bc862c25ab5a1ee10d40
SuuCH <ntrikrik1027@gmail.com>
2021-11-16 01:59:40 +0900

    [feat] 本日のタスクパネルの追加

--- frontend/src/pages/top/index.tsx
+++ frontend/src/pages/top/index.tsx
@@ -1,6 +1,7 @@
 import type { VFC } from "react";
 import { Calendar } from "../../components/top/Calendar";
 import { Navber } from "../../components/top/Navber";
+import { TaskPanel } from "../../components/top/TaskPanel";
 import { TaskTabs } from "../../components/top/TaskTabs";
 
 const Top: VFC = () => {
@@ -8,6 +9,7 @@ const Top: VFC = () => {
     <>
       <Navber />
       <Calendar />
+      <TaskPanel />
       <TaskTabs />
     </>
   );
