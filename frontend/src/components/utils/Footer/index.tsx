6ef827a34c2d5ab35215052b4e24608410fd77ee
SuuCH <ntrikrik1027@gmail.com>
2021-11-16 02:30:24 +0900

    [feat] フッターの追加

---
+++ frontend/src/components/utils/Footer/index.tsx
@@ -0,0 +1,11 @@
+import type { VFC } from "react";
+
+const Footer: VFC = () => {
+  return (
+    <footer style={{ textAlign: "center", color: "gray", fontSize: "15px" }}>
+      © 2021 RikuFujii(SuuCH),halpi(halpi17)
+    </footer>
+  );
+};
+
+export { Footer };
--- frontend/src/pages/top/index.tsx
+++ frontend/src/pages/top/index.tsx
@@ -1,9 +1,11 @@
 import type { VFC } from "react";
+import { Footer } from "../../components/utils/Footer";
 
 const Top: VFC = () => {
   return (
     <>
       <div>aiueo</div>
+      <Footer />
     </>
   );
 };
