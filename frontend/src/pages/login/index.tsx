01da649fcb7fadce6cbd32de6f9ab6da86bfdc45
halpi17 <nishiyamaymyt@gmail.com>
2021-11-21 01:54:59 +0900

    [refactor] ログインボタンコンポーネントの削除

--- frontend/src/pages/login/index.tsx
+++ frontend/src/pages/login/index.tsx
@@ -1,19 +1,11 @@
-import { BaseButton } from "../../components/utils/BaseButton";
 import { Form } from "../../components/login/Form";
 import { MainPanel } from "../../components/login/MainPanel";
 import type { VFC } from "react";
-import { useNavigate } from "react-router-dom";
 import styles from "./index.module.css";
 import LogoTaskusa from "../../images/LogoTaskusaNotMargin.png";
 import { Footer } from "../../components/utils/Footer";
 
 const Login: VFC = () => {
-  const navigate = useNavigate();
-
-  const handleOnClickLoginButton = (): void => {
-    console.log("push");
-    navigate("/top");
-  };
   return (
     <>
       <div className={styles.panel}>
@@ -22,14 +14,6 @@ const Login: VFC = () => {
             <img className={styles.image} src={LogoTaskusa} alt="logo" />
           </div>
           <Form />
-          <div className={styles.buttonArea}>
-            <BaseButton
-              className={styles.button}
-              text="ログイン"
-              color="#06681B"
-              onClick={handleOnClickLoginButton}
-            />
-          </div>
         </MainPanel>
       </div>
       <Footer />
