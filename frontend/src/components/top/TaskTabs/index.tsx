2ba984e6185f6e45e731c3f07d91cf2d5e9af11f
SuuCH <ntrikrik1027@gmail.com>
2021-11-20 19:52:25 +0900

    [feat] TaskPanelにPropsを渡す

--- frontend/src/components/top/TaskTabs/index.tsx
+++ frontend/src/components/top/TaskTabs/index.tsx
@@ -12,6 +12,7 @@ import {
   ThemeProvider,
 } from "@material-ui/core";
 import { TabPanel } from "../TabPanel";
+import { TaskPanel } from "../TaskPanel";
 
 const useStyles = makeStyles({
   root: {
@@ -37,76 +38,112 @@ const theme = createTheme({
 });
 
 const TaskTabs: VFC = () => {
+  const dummyData = [
+    { id: 1, content: "あいうえお" },
+    { id: 2, content: "かきくけこ" },
+    { id: 3, content: "さしすせそ" },
+  ];
   const classes = useStyles();
   const [value, setValue] = useState(0);
 
   const handleChange = (event: ChangeEvent<{}>, newValue: number) => {
     setValue(newValue);
   };
-
+  const handleOnClickToTodayButton = (): void => {
+    console.log("今日へ押した");
+  };
+  const handleOnClickUndoButton = (): void => {
+    console.log("元に戻す押した");
+  };
+  const handleOnClickDeleteButton = (): void => {
+    console.log("削除押した");
+  };
   return (
-    <ThemeProvider theme={theme}>
-      <Paper square className={classes.root}>
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          variant="fullWidth"
-          TabIndicatorProps={{ style: { background: "#8EF3AA" } }}
-        >
-          <Tab
-            label={
-              <div>
-                <CheckBoxOutlineBlankOutlinedIcon
-                  style={{
-                    verticalAlign: "middle",
-                    marginBottom: 2,
-                    marginRight: 6,
-                  }}
-                />
-                未完了
-              </div>
-            }
-          />
-          <Tab
-            label={
-              <div>
-                <ListAltOutlinedIcon
-                  style={{
-                    verticalAlign: "middle",
-                    marginBottom: 2,
-                    marginRight: 6,
-                  }}
-                />
-                予定
-              </div>
-            }
-          />
-          <Tab
-            label={
-              <div>
-                <CheckBoxOutlinedIcon
-                  style={{
-                    verticalAlign: "middle",
-                    marginBottom: 2,
-                    marginRight: 6,
-                  }}
-                />
-                完了済
-              </div>
-            }
-          />
-        </Tabs>
-        <TabPanel value={value} index={0}>
-          Item One
-        </TabPanel>
-        <TabPanel value={value} index={1}>
-          Item Two
-        </TabPanel>
-        <TabPanel value={value} index={2}>
-          Item Three
-        </TabPanel>
-      </Paper>
-    </ThemeProvider>
+    <div style={{marginBottom: "200px"}}>
+      <ThemeProvider theme={theme}>
+        <Paper square className={classes.root}>
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            variant="fullWidth"
+            TabIndicatorProps={{ style: { background: "#8EF3AA" } }}
+          >
+            <Tab
+              label={
+                <div>
+                  <CheckBoxOutlineBlankOutlinedIcon
+                    style={{
+                      verticalAlign: "middle",
+                      marginBottom: 2,
+                      marginRight: 6,
+                    }}
+                  />
+                  未完了
+                </div>
+              }
+            />
+            <Tab
+              label={
+                <div>
+                  <ListAltOutlinedIcon
+                    style={{
+                      verticalAlign: "middle",
+                      marginBottom: 2,
+                      marginRight: 6,
+                    }}
+                  />
+                  予定
+                </div>
+              }
+            />
+            <Tab
+              label={
+                <div>
+                  <CheckBoxOutlinedIcon
+                    style={{
+                      verticalAlign: "middle",
+                      marginBottom: 2,
+                      marginRight: 6,
+                    }}
+                  />
+                  完了済
+                </div>
+              }
+            />
+          </Tabs>
+          <TabPanel value={value} index={0}>
+            <TaskPanel
+              title="未完了のたすく！"
+              taskData={dummyData}
+              buttonLabel1="今日へ"
+              buttonColor1="#104F1B"
+              onClick1={handleOnClickToTodayButton}
+              onClick2={handleOnClickDeleteButton}
+            />
+          </TabPanel>
+          <TabPanel value={value} index={1}>
+            <TaskPanel
+              title="2021年11月20日（土）のたすく！"
+              taskData={dummyData}
+              buttonLabel1="今日へ"
+              buttonColor1="#104F1B"
+              onClick1={handleOnClickToTodayButton}
+              onClick2={handleOnClickDeleteButton}
+            />
+          </TabPanel>
+          <TabPanel value={value} index={2}>
+            <TaskPanel
+              title="完了済みのたすく！"
+              taskData={dummyData}
+              buttonLabel1="元に戻す"
+              buttonColor1="#69A41F"
+              onClick1={handleOnClickUndoButton}
+              onClick2={handleOnClickDeleteButton}
+            />
+          </TabPanel>
+        </Paper>
+      </ThemeProvider>
+    </div>
   );
 };
 
--- frontend/src/pages/top/index.tsx
+++ frontend/src/pages/top/index.tsx
@@ -4,14 +4,34 @@ import { Calendar } from "../../components/top/Calendar";
 import { Navber } from "../../components/top/Navber";
 import { TaskPanel } from "../../components/top/TaskPanel";
 import { TaskTabs } from "../../components/top/TaskTabs";
+import styles from "./index.module.css";
 
 const Top: VFC = () => {
+  const dummyData = [
+    { id: 1, content: "あいうえお" },
+    { id: 2, content: "かきくけこ" },
+    { id: 3, content: "さしすせそ" },
+  ];
+  const handleOnClickCompleteButton = (): void => {
+    console.log("完了押した");
+  };
+  const handleOnClickDeleteButton = (): void => {
+    console.log("削除押した");
+  };
   return (
     <>
       <Navber />
       <Calendar />
       <AddTaskForm />
-      <TaskPanel />
+      <TaskPanel
+        title="本日のたすく！"
+        taskData={dummyData}
+        buttonLabel1="完了"
+        buttonColor1="#24C075"
+        className={styles.todaysTaskPanel}
+        onClick1={handleOnClickCompleteButton}
+        onClick2={handleOnClickDeleteButton}
+      />
       <TaskTabs />
     </>
   );
