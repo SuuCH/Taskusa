ce6f081c4c3dff11d34992c90e057253eefec433
SuuCH <ntrikrik1027@gmail.com>
2021-12-08 15:07:49 +0900

    [feat] タスク型をstringに変更

--- frontend/src/components/top/TaskPanel/index.tsx
+++ frontend/src/components/top/TaskPanel/index.tsx
@@ -4,21 +4,16 @@ import { List, ListItem } from "@material-ui/core";
 import styles from "./index.module.css";
 import { BaseButton } from "../../utils/BaseButton";
 
-interface Task {
-  id: number;
-  content: string;
-}
-
 interface Props {
   title: string;
-  taskData: Task[];
+  taskData: string[];
   buttonLabel1: string;
   buttonLabel2?: string;
   buttonColor1: string;
   buttonColor2?: string;
   className?: string;
   onClick1: MouseEventHandler<HTMLButtonElement>;
-  onClick2: MouseEventHandler<HTMLButtonElement>;
+  onClick2: (idx: number) => void;
 }
 
 const TaskPanel: VFC<Props> = ({
@@ -28,38 +23,35 @@ const TaskPanel: VFC<Props> = ({
   buttonLabel2 = "削除",
   buttonColor1,
   buttonColor2 = "#E73152",
-  className="",
+  className = "",
   onClick1,
   onClick2,
 }: Props) => {
-  const TaskListItem = (datas: Task[]): JSX.Element[] => {
-    return datas.map((data) => {
-      return (
-        <ListItem style={{ paddingLeft: "0px" }}>
-          <p className={styles.listItemText}>{data.content}</p>
-          <div className={styles.taskButtonWrapper}>
-            <div className={styles.completeButton}>
-              <BaseButton
-                color={buttonColor1}
-                onClick={onClick1}
-                text={buttonLabel1}
-              />
-            </div>
-            <BaseButton
-              color={buttonColor2}
-              onClick={onClick2}
-              text={buttonLabel2}
-            />
-          </div>
-        </ListItem>
-      );
-    });
-  };
   return (
     <>
-      <div className={clsx(styles.panel,className)}>
+      <div className={clsx(styles.panel, className)}>
         <p className={styles.panelTitle}>{title}</p>
-        <List style={{ fontSize: "20px" }}>{TaskListItem(taskData)}</List>
+        <List style={{ fontSize: "20px" }}>
+          {taskData.map((task, index) => (
+            <ListItem style={{ paddingLeft: "0px" }}>
+              <p className={styles.listItemText}>{task}</p>
+              <div className={styles.taskButtonWrapper}>
+                <div className={styles.completeButton}>
+                  <BaseButton
+                    color={buttonColor1}
+                    onClick={onClick1}
+                    text={buttonLabel1}
+                  />
+                </div>
+                <BaseButton
+                  color={buttonColor2}
+                  onClick={() => onClick2(index)}
+                  text={buttonLabel2}
+                />
+              </div>
+            </ListItem>
+          ))}
+        </List>
       </div>
     </>
   );
