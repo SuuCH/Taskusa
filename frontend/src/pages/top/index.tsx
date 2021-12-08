c731dee928fa3a77e24c0a926d3cd614d816d578
SuuCH <ntrikrik1027@gmail.com>
2021-12-08 15:07:21 +0900

    [feat] タスク入力時、追加、削除の処理の実装

--- frontend/src/pages/top/index.tsx
+++ frontend/src/pages/top/index.tsx
@@ -1,32 +1,47 @@
-import type { VFC } from "react";
+import { useState } from "react";
 import { AddTaskForm } from "../../components/top/AddTaskForm";
 import { Calendar } from "../../components/top/Calendar";
 import { Navber } from "../../components/top/Navber";
 import { TaskPanel } from "../../components/top/TaskPanel";
 import { TaskTabs } from "../../components/top/TaskTabs";
 import { Footer } from "../../components/utils/Footer";
+
+import type { ChangeEvent, VFC } from "react";
+
 import styles from "./index.module.css";
 
 const Top: VFC = () => {
-  const dummyData = [
-    { id: 1, content: "あいうえお" },
-    { id: 2, content: "かきくけこ" },
-    { id: 3, content: "さしすせそ" },
-  ];
+  const [input, setInput] = useState("");
+  const [todoList, setTodoList] = useState([] as string[]);
+  const handleChangeTaskInputForm = (
+    e: ChangeEvent<HTMLInputElement>
+  ): void => {
+    setInput(e.target.value);
+  };
+  const handleClickTaskAddButton = (): void => {
+    setTodoList([...todoList, input]);
+    setInput("");
+  };
+
   const handleOnClickCompleteButton = (): void => {
     console.log("完了押した");
   };
-  const handleOnClickDeleteButton = (): void => {
-    console.log("削除押した");
+  const handleOnClickDeleteButton = (index: number): void => {
+    setTodoList(todoList.filter((_, idx) => idx !== index));
   };
+
   return (
     <>
       <Navber />
       <Calendar />
-      <AddTaskForm />
+      <AddTaskForm
+        input={input}
+        onChange={handleChangeTaskInputForm}
+        onClick={handleClickTaskAddButton}
+      />
       <TaskPanel
         title="本日のたすく！"
-        taskData={dummyData}
+        taskData={todoList}
         buttonLabel1="完了"
         buttonColor1="#24C075"
         className={styles.todaysTaskPanel}
