585282d882fd2f9bcf75d6b4da8437b73313a534
SuuCH <ntrikrik1027@gmail.com>
2021-12-09 17:02:09 +0900

    [chore] ダミーデータをよりリアルに変更

--- frontend/src/components/top/TaskTabs/index.tsx
+++ frontend/src/components/top/TaskTabs/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import type { ChangeEvent, VFC } from "react";
+import type { Task } from "../../../types/task";
 import CheckBoxOutlineBlankOutlinedIcon from "@material-ui/icons/CheckBoxOutlineBlankOutlined";
 import ListAltOutlinedIcon from "@material-ui/icons/ListAltOutlined";
 import CheckBoxOutlinedIcon from "@material-ui/icons/CheckBoxOutlined";
@@ -38,13 +39,26 @@ const theme = createTheme({
 });
 
 interface Props {
-  finishedList: (string | undefined)[];
+  finishedList: (Task | undefined)[];
   onClick5: (idx: number) => void;
   onClick6: (idx: number) => void;
 }
 
 const TaskTabs: VFC<Props> = ({ finishedList, onClick5, onClick6 }: Props) => {
-  const dummyData = ["あいうえお", "かきくけこ", "さしすせそ"];
+  const dummyData = [
+    {
+      task: "コーディング試験受ける",
+      createdAt: new Date(),
+    },
+    {
+      task: "TaskusaのFirestore繋ぎこみ実装",
+      createdAt: new Date(),
+    },
+    {
+      task: "収集くん開発（FastAPI）",
+      createdAt: new Date(),
+    },
+  ];
   const classes = useStyles();
   const [value, setValue] = useState(0);
 
@@ -132,7 +146,7 @@ const TaskTabs: VFC<Props> = ({ finishedList, onClick5, onClick6 }: Props) => {
           </TabPanel>
           <TabPanel value={value} index={1}>
             <TaskPanel
-              title="2021年11月20日（土）のたすく！"
+              title="2021年12月10日（金）のたすく！"
               taskData={dummyData}
               buttonLabel1="今日へ"
               buttonColor1="#104F1B"
