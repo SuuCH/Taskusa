676de7c3f5007252abf7700e3a3a9cdfdd6a2dfd
RikuFujii <60252699+SuuCH@users.noreply.github.com>
2021-12-09 09:57:27 +0900

    [feat] ログアウト処理の実装 (#48)

--- frontend/src/components/top/Navber/index.tsx
+++ frontend/src/components/top/Navber/index.tsx
@@ -2,13 +2,15 @@ import type { VFC } from "react";
 import { AppBar, Box, Toolbar } from "@material-ui/core";
 import { BaseButton } from "../../utils/BaseButton";
 import { useNavigate } from "react-router-dom";
+
+import logout from "../../../api/authentication/logout";
 import LogoTaskusa from "../../../images/LogoTaskusa.png";
 import styles from "./index.module.css";
 
 const Navber: VFC = () => {
   const navigate = useNavigate();
   const handleOnClickLogoutButton = (): void => {
-    console.log("push");
+    logout();
     navigate("/");
   };
   return (
@@ -20,7 +22,7 @@ const Navber: VFC = () => {
             className={styles.navber}
           >
             <img src={LogoTaskusa} alt="TaskusaLogo" className={styles.image} />
-            <div className= {styles.button}>
+            <div className={styles.button}>
               <BaseButton
                 text="ログアウト"
                 color="#06681B"
