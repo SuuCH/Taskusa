20e1fec7062cad2068847a5ddc832dbbbc19192c
SuuCH <ntrikrik1027@gmail.com>
2021-12-23 04:50:23 +0900

    [feat] switchで本日とそれ以外のタスクで振り分け

--- frontend/src/components/top/TaskPanel/index.tsx
+++ frontend/src/components/top/TaskPanel/index.tsx
@@ -6,6 +6,7 @@ import styles from "./index.module.css";
 import { BaseButton } from "../../utils/BaseButton";
 
 interface Props {
+  tabType: "today" | "notComplete" | "plan" | "complete";
   title: string;
   taskData: (Task | undefined)[];
   buttonLabel1: string;
@@ -18,6 +19,7 @@ interface Props {
 }
 
 const TaskPanel: VFC<Props> = ({
+  tabType,
   title,
   taskData,
   buttonLabel1,
@@ -28,30 +30,119 @@ const TaskPanel: VFC<Props> = ({
   onClick1,
   onClick2,
 }: Props) => {
+  const todaysDate = new Date();
+  const formatTodaysDate = {
+    year: todaysDate.getFullYear(),
+    month: todaysDate.getMonth(),
+    date: todaysDate.getDate(),
+  };
   return (
     <>
       <div className={clsx(styles.panel, className)}>
         <p className={styles.panelTitle}>{title}</p>
         <List style={{ fontSize: "20px" }}>
-          {taskData.map((task, index) => (
-            <ListItem style={{ paddingLeft: "0px" }}>
-              <p className={styles.listItemText}>{task?.task}</p>
-              <div className={styles.taskButtonWrapper}>
-                <div className={styles.completeButton}>
-                  <BaseButton
-                    color={buttonColor1}
-                    onClick={() => onClick1(index)}
-                    text={buttonLabel1}
-                  />
-                </div>
-                <BaseButton
-                  color={buttonColor2}
-                  onClick={() => onClick2(index)}
-                  text={buttonLabel2}
-                />
-              </div>
-            </ListItem>
-          ))}
+          {(() => {
+            switch (tabType) {
+              case "today":
+                return taskData.map((task, index) => {
+                  if (task === undefined) {
+                    return null;
+                  } else {
+                    if (
+                      new Date(task.createdAt).getFullYear() ===
+                        formatTodaysDate.year &&
+                      new Date(task.createdAt).getMonth() ===
+                        formatTodaysDate.month &&
+                      new Date(task.createdAt).getDate() ===
+                        formatTodaysDate.date
+                    ) {
+                      return (
+                        <ListItem style={{ paddingLeft: "0px" }}>
+                          <p className={styles.listItemText}>{task?.task}</p>
+                          <div className={styles.taskButtonWrapper}>
+                            <div className={styles.completeButton}>
+                              <BaseButton
+                                color={buttonColor1}
+                                onClick={() => onClick1(index)}
+                                text={buttonLabel1}
+                              />
+                            </div>
+                            <BaseButton
+                              color={buttonColor2}
+                              onClick={() => onClick2(index)}
+                              text={buttonLabel2}
+                            />
+                          </div>
+                        </ListItem>
+                      );
+                    } else {
+                      return null;
+                    }
+                  }
+                });
+              case "notComplete":
+                return taskData.map((task, index) => {
+                  if (task === undefined) {
+                    return null;
+                  } else {
+                    if (
+                      new Date(task.createdAt).getFullYear() !==
+                        formatTodaysDate.year &&
+                      new Date(task.createdAt).getMonth() !==
+                        formatTodaysDate.month &&
+                      new Date(task.createdAt).getDate() !==
+                        formatTodaysDate.date
+                    ) {
+                      return (
+                        <ListItem style={{ paddingLeft: "0px" }}>
+                          <p className={styles.listItemText}>{task?.task}</p>
+                          <div className={styles.taskButtonWrapper}>
+                            <div className={styles.completeButton}>
+                              <BaseButton
+                                color={buttonColor1}
+                                onClick={() => onClick1(index)}
+                                text={buttonLabel1}
+                              />
+                            </div>
+                            <BaseButton
+                              color={buttonColor2}
+                              onClick={() => onClick2(index)}
+                              text={buttonLabel2}
+                            />
+                          </div>
+                        </ListItem>
+                      );
+                    } else {
+                      return null;
+                    }
+                  }
+                });
+              case "plan":
+                return null;
+              case "complete":
+                return taskData.map((task, index) => (
+                  <ListItem style={{ paddingLeft: "0px" }}>
+                    <p className={styles.listItemText}>{task?.task}</p>
+                    <div className={styles.taskButtonWrapper}>
+                      <div className={styles.completeButton}>
+                        <BaseButton
+                          color={buttonColor1}
+                          onClick={() => onClick1(index)}
+                          text={buttonLabel1}
+                        />
+                      </div>
+                      <BaseButton
+                        color={buttonColor2}
+                        onClick={() => onClick2(index)}
+                        text={buttonLabel2}
+                      />
+                    </div>
+                  </ListItem>
+                ));
+              default:
+                return null;
+            }
+          })()}
         </List>
       </div>
     </>
